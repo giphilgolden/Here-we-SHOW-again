643cf6fff3f2404259b91adb7815f0a35f850c1a
giphilgolden <62906944+giphilgolden@users.noreply.github.com>
2020-06-15 14:03:44 -0400

    inintialish commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let mainElement = document.querySelector("main")
+let kata1Heading  = document.createElement("h1")
+let kata2Heading = document.createElement("h1")
+
+mainElement.append(kata1Heading)
+kata1Heading.append("kata 1")
+
+mainElement.append(kata2Heading)
+kata1Heading.append("kata 2")
